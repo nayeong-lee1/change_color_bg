4d72d3b04f276257dfe0f005513fb8c94c7e00ba
nayeong-lee1 <nayeonglee1@gmail.com>
2020-02-18 20:21:07 -0400

    put js code to loop through endlessly

--- change_color_bg/script.js
+++ change_color_bg/script.js
@@ -1,10 +1,15 @@
 var body = document.querySelector('body');
 var button = document.getElementById('button');
 var colors = ['powderblue', 'red', 'pink', 'yellow', 'brown'];
+var i = 0;
 
 function changeColor () {
-	var color = colors.shift();
-	body.style.backgroundColor = color;
+
+body.style.backgroundColor = colors[i];
+i++;
+if(i === colors.length){
+	i=0;
+}
 }
 
 button.addEventListener("click", changeColor)
